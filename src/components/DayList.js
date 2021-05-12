5c3e31cad476a45d0eee9ce3b940b1a32cc46431
sukick <suki.ckchan@gmail.com>
2021-05-12 02:44:51 -0700

    Build DayList, add state

---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import DayListItem from "components/DayListItem"
+
+export default function DayList(props) {
+
+  const Days = props.days.map((day) => {
+    return (
+      <DayListItem 
+      key={day.id}
+      id={day.id}
+      name={day.name}
+      spots={day.spots} 
+      selected={day.name === props.day}
+      setDay={props.setDay}
+      />
+    )
+  })
+
+  return (
+    <ul>{Days}</ul>
+  );
+} 
