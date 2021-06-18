995410ab31a85532c3ecb492ba0425868a5e30f5
sukick <suki.ckchan@gmail.com>
2021-06-17 18:36:11 -0700

    Implement create and delete appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -30,6 +30,49 @@ export default function Application(props) {
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day)
 
+  function bookInterview(id, interview) {
+    console.log(id, interview);
+
+  const appointment ={
+    ...state.appointments[id],
+    interview: {...interview}
+  };
+
+  const appointments = {
+    ...state.appointments,
+    [id]: appointment
+  };
+
+  return axios.put(`/api/appointments/${id}`, { interview })
+    .then(() => {
+      setState({ ...state, appointments });
+    });
+  }
+
+  function cancelInterview(id) {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.delete(`api/appointments/${id}`)
+      .then(() => setState({ ...state, appointments}))
+
+  };
+
+//   setState({
+//     ...state,
+//     appointments
+//   });
+
+// }
+
   const schedule = appointments.map((appointment) => {
 
     const interview = getInterview(state, appointment.interview);
@@ -41,6 +84,8 @@ export default function Application(props) {
     time = {appointment.time}
     interview = {interview}
     interviewers={interviewers}
+    bookInterview={bookInterview}
+    cancelInterview={cancelInterview}
     />
    );
 });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,11 +4,19 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
+import Confirm from "components/Appointment/Confirm";
+import Error from "components/Appointment/Error";
 import useVisualMode from "hooks/useVisualMode";
+
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
+const CONFIRM = "CONFIRM";
+const DELETING = "DELETING";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
    // console.log("props interviewer", props.interviewer)
@@ -16,25 +24,55 @@ export default function Appointment(props) {
    const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
     );
-  
+    
+    function save(name, interviewer) {
+      const interview = {
+        student: name,
+        interviewer
+      };
+
+      transition("SAVING");
+      props.bookInterview(props.id, interview)
+        .then(() => transition("SHOW"))
+    }
+
+    function deleteInterview() {
+      transition("DELETING")
+      props.cancelInterview(props.id)
+        .then(() => transition("EMPTY"))
+    }
+
     return (
       <article className="appointment">
         <Header time={props.time} />
   
         {mode === EMPTY && <Empty onAdd={() => transition("CREATE")} />}
-        {mode === SHOW && (
+        {mode === SHOW && props.interview && (
           <Show
             student={props.interview.student}
+            interview={props.interview}
             interviewer={props.interview.interviewer}
+            onDelete={() => transition("CONFIRM")}
           />
         )}
         {mode === CREATE && (
           <Form 
            interviewers={props.interviewers}
+           onSave={save}
            onCancel={back}
   
           />
         )}
+         {mode === SAVING && <Status message="Saving" />}
+         {mode === DELETING && <Status message="Deleting" />}
+         {mode === CONFIRM && (
+        <Confirm
+          message= {"Are you sure you would like to delete?"}
+          onConfirm= {deleteInterview}
+          onCancel= {back}
+          message= {"Are you sure you would like to delete?"}
+        />
+      )}
   
   
       </article>
